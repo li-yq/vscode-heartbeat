6a7adea04dd8a58e12ddd47b8f20a1f1a1f86820
Li Yuanqing <39945542+li-yq@users.noreply.github.com>
2024-02-01 14:42:54 +0000

    🔥 remove command example

--- src/extension.ts
+++ src/extension.ts
@@ -10,16 +10,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "heartbeat" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('heartbeat.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Heartbeat!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
